7a302462b1dd3c31bbfd703205e5cfa651d4bf11
Aleksander Lewicki <aleksanderlewy218@gmail.com>
2023-12-24 03:07:13 +0100

    Added Charts v2

--- my-react-app/src/components/AdminPage.js
+++ my-react-app/src/components/AdminPage.js
@@ -76,6 +76,7 @@ function Admin() {
 
   return (
     <div className='Page-container-Admin'>
+
       <div className='Header-Admin'>
         Witaj w Panelu Administracyjnym Ankiet Studenckich
       <button onClick={handleHome} className="SurveyButton">
@@ -85,40 +86,141 @@ function Admin() {
         Wyloguj się
       </button>
       </div>
-    <div className="surveys-container-Admin">
-            Wyszukaj Wyniki Ankiet po Semestrze i Grupie Studenckiej!
-            <input type="number" placeholder="Semestr" value={semester} onChange={handleSemesterChange} />
-            <input type="" placeholder="Grupa" value={groupName} onChange={handleGroupChange} />
-            <button onClick={fetchSurveyChartDataForAllOfferings}>Pobierz Wyniki Ankiet</button>
-            {allChartData.map((chartData, index) => {
+    
+
+      <div className='MiddleContainer'>
+      <div className="title-container">
+        Wyszukaj Wyniki Ankiet po Semestrze i Grupie Studenckiej!
+      </div>
+      <div className="inputs-container">
+        <input type="number" placeholder="Semestr" value={semester} onChange={handleSemesterChange} />
+        <input type="" placeholder="Grupa Studencka" value={groupName} onChange={handleGroupChange} />
+      </div>
+      <div className="button-container">
+        <button onClick={fetchSurveyChartDataForAllOfferings}>Pobierz Wyniki Ankiet</button>
+      </div>
+      </div>
+
+      <div className="surveys-container-Admin">
+      {allChartData.map((chartData, index) => {
           
           const answer1Occurrences = countOccurrences(chartData.Answer1Data);
-          
-          const chartDataArray = Array.from({ length: 5 }, (_, i) => answer1Occurrences[i + 1] || 0);
+          const answer2Occurrences = countOccurrences(chartData.Answer2Data);
+          const answer3Occurrences = countOccurrences(chartData.Answer3Data);
+          const answer4Occurrences = countOccurrences(chartData.Answer4Data);
+          const answer5Occurrences = countOccurrences(chartData.Answer5Data);
+          const answer6Occurrences = countOccurrences(chartData.Answer6Data);
+          const answer7Occurrences = countOccurrences(chartData.Answer7Data);
+        
+          const chartDataArray1 = Array.from({ length: 5 }, (_, i) => answer1Occurrences[i + 1] || 0);
+          const chartDataArray2 = Array.from({ length: 5 }, (_, i) => answer2Occurrences[i + 1] || 0);
+          const chartDataArray3 = Array.from({ length: 5 }, (_, i) => answer3Occurrences[i + 1] || 0);
+          const chartDataArray4 = Array.from({ length: 5 }, (_, i) => answer4Occurrences[i + 1] || 0);
+          const chartDataArray5 = Array.from({ length: 5 }, (_, i) => answer5Occurrences[i + 1] || 0);
+          const chartDataArray6 = Array.from({ length: 5 }, (_, i) => answer6Occurrences[i + 1] || 0);
+          const chartDataArray7 = Array.from({ length: 5 }, (_, i) => answer7Occurrences[i + 1] || 0);
+
           return (
             <div key={index}>
-              <h2>{chartData.SubjectName} Prowadzone przez {chartData.InstructorTitle} {chartData.InstructorName} Rodzaj Zajęć: {chartData.ClassTypeName} </h2>
-              <div>
+              <h2>{chartData.SubjectName} prowadzone/a przez {chartData.InstructorTitle} {chartData.InstructorName} Rodzaj Zajęć: {chartData.ClassTypeName} </h2>
+              <div style={{ width: '98%', height: '100%'}} >
               <Bar
                 data={{
                   labels: ['1', '2', '3', '4', '5'],
                   datasets: [
                     {
                       label: 'Wyniki',
-                      data: chartDataArray,
+                      data: chartDataArray1,
                       backgroundColor: 'rgba(53, 162, 235, 0.5)',
                     },
-                    //Dalsze Wykresy
                   ],
                 }}
-                options = {createChartOptions('Jak ogólnie oceniasz zajęcia w skali od 1 do 5?')}
+                options = {createChartOptions('Pytanie 1: Jak ogólnie oceniasz zajęcia w skali od 1 do 5?')}
+              />
+              <Bar
+                data={{
+                  labels: ['1', '2', '3', '4', '5'],
+                  datasets: [
+                    {
+                      label: 'Wyniki',
+                      data: chartDataArray2,
+                      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    },
+                  ],
+                }}
+                options = {createChartOptions('Stwierdzenie 1. Na pierwszych zajęciach osoba prowadząca określiła, zgodnie z sylabusem, założenia i zasady zaliczania przedmiotu.')}
+              />
+              <Bar
+                data={{
+                  labels: ['1', '2', '3', '4', '5'],
+                  datasets: [
+                    {
+                      label: 'Wyniki',
+                      data: chartDataArray3,
+                      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    },
+                  ],
+                }}
+                options = {createChartOptions('Stwierdzenie 2. Uzyskano od osoby prowadzącej odpowiedzi na pytania zadawane w czasie zajęć i/lub konsultacji.')}
+              />
+               <Bar
+                data={{
+                  labels: ['1', '2', '3', '4', '5'],
+                  datasets: [
+                    {
+                      label: 'Wyniki',
+                      data: chartDataArray4,
+                      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    },
+                  ],
+                }}
+                options = {createChartOptions('Stwierdzenie 3. Osoby studiujące były traktowane przez osobę prowadzącą z szacunkiem i zgodnie z zasadą równego traktowania.')}
+              />
+              <Bar
+                data={{
+                  labels: ['1', '2', '3', '4', '5'],
+                  datasets: [
+                    {
+                      label: 'Wyniki',
+                      data: chartDataArray5,
+                      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    },
+                  ],
+                }}
+                options = {createChartOptions('Stwierdzenie 4. Określone w sylabusie zasady zaliczenia przedmiotu były przestrzegane.')}
+              />
+              <Bar
+                data={{
+                  labels: ['1', '2', '3', '4', '5'],
+                  datasets: [
+                    {
+                      label: 'Wyniki',
+                      data: chartDataArray6,
+                      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    },
+                  ],
+                }}
+                options = {createChartOptions('Stwierdzenie 5. Prezentowany przez osobę prowadzącą materiał rozwinął moją wiedzę i/lub umiejętności.')}
+              />
+              <Bar
+                data={{
+                  labels: ['1', '2', '3', '4', '5'],
+                  datasets: [
+                    {
+                      label: 'Wyniki',
+                      data: chartDataArray7,
+                      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    },
+                  ],
+                }}
+                options = {createChartOptions('Stwierdzenie 6. Zajęcia oceniam jako interesujące.')}
               />
               </div>
             </div>
           );
         })}
     </div>
-    </div>
+  </div>
   );
 };
 
